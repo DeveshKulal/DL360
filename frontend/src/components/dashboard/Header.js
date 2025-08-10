a14158a174da28115525aa10a71b95b9131512c5
DeveshKulal <deveshkulal27@gmail.com>
2025-08-10 15:21:01 +0530

    Changes are done in the side bar for responsive UI

--- frontend/src/components/dashboard/Header.js
+++ frontend/src/components/dashboard/Header.js
@@ -5,14 +5,8 @@ const Header = () => {
   return (
     <div className="flex justify-between items-center px-16 py-4 bg-white shadow-sm">
       <h2 className="text-xl font-semibold">Dashboard</h2>
-      <div className="flex items-center gap-4">
+      <div className="flex items-end  ">
         <Bell className="text-gray-600" />
-        <div className="avatar online">
-          <div className="w-8 rounded-full">
-
-          </div>
-        </div>
-
       </div>
     </div>
   );
